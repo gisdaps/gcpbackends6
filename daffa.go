42a9e6d5f0620660d90fbfd3fa2460736bea23fb
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-11-12 18:27:16 +0700

    update backend

--- daffa.go
+++ daffa.go
@@ -1,37 +1,133 @@
 package gcpbackends6
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
+
+	pasproj "github.com/HRMonitorr/PasetoprojectBackend"
+	"github.com/petapedia/peda"
+	"github.com/whatsauth/watoken"
 )
 
-func GCHandlerFunc(Mongostring, dbname, colname string) string {
-	koneksyen := GetConnectionMongo(Mongostring, dbname)
-	datageo := GetAllData(koneksyen, colname)
-
-	jsoncihuy, _ := json.Marshal(datageo)
-
-	return string(jsoncihuy)
+func GCHandlerFunc(publickey, Mongostring, dbname, colname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		existing := IsExist(tokenlogin, os.Getenv(publickey))
+		if !existing {
+			resp.Status = false
+			resp.Message = "Kamu kayaknya belum punya akun"
+		} else {
+			koneksyen := GetConnectionMongo(Mongostring, dbname)
+			datageo := GetAllGeoData(koneksyen, colname)
+			jsoncihuy, _ := json.Marshal(datageo)
+			resp.Status = true
+			resp.Message = "Data Berhasil diambil"
+			resp.Token = string(jsoncihuy)
+		}
+	}
+	return ReturnStringStruct(resp)
 }
 
-func GCFPostCoordinate(Mongostring, dbname, colname string, r *http.Request) string {
+func GCFPostCoordinate(Mongostring, Publickey, dbname, colname string, r *http.Request) string {
 	req := new(Credents)
 	conn := GetConnectionMongo(Mongostring, dbname)
 	resp := new(LonLatProperties)
 	err := json.NewDecoder(r.Body).Decode(&resp)
-	if err != nil {
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
 		req.Status = strconv.Itoa(http.StatusNotFound)
-		req.Message = "error parsing application/json: " + err.Error()
+		req.Message = "Header Login Not Exist"
 	} else {
-		req.Status = strconv.Itoa(http.StatusOK)
-		Ins := InsertDataGeojson(conn, colname,
-			resp.Coordinates,
-			resp.Name,
-			resp.Volume,
-			resp.Type)
-		req.Message = fmt.Sprintf("%v:%v", "Berhasil Input data", Ins)
+		existing := IsExist(tokenlogin, os.Getenv(Publickey))
+		if !existing {
+			req.Status = strconv.Itoa(http.StatusNotFound)
+			req.Message = "Kamu kayaknya belum punya akun"
+		} else {
+			if err != nil {
+				req.Status = strconv.Itoa(http.StatusNotFound)
+				req.Message = "error parsing application/json: " + err.Error()
+			} else {
+				req.Status = strconv.Itoa(http.StatusOK)
+				Ins := InsertDataLonlat(conn, colname,
+					resp.Coordinates,
+					resp.Name,
+					resp.Volume,
+					resp.Type)
+				req.Message = fmt.Sprintf("%v:%v", "Berhasil Input data", Ins)
+			}
+		}
+	}
+	return ReturnStringStruct(req)
+}
+
+func GCFUpdateName(publickey, Mongostring, dbname string, r *http.Request) string {
+	req := new(Credents)
+	resp := new(LonLatProperties)
+	err := json.NewDecoder(r.Body).Decode(&resp)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		req.Status = strconv.Itoa(http.StatusNotFound)
+		req.Message = "Header Login Not Exist"
+	} else {
+		existing := IsExist(tokenlogin, os.Getenv(publickey))
+		if !existing {
+			req.Status = strconv.Itoa(http.StatusNotFound)
+			req.Message = "Kamu kayaknya belum punya akun"
+		} else {
+			if err != nil {
+				req.Status = strconv.Itoa(http.StatusNotFound)
+				req.Message = "error parsing application/json: " + err.Error()
+			} else {
+				req.Status = strconv.Itoa(http.StatusOK)
+				Ins := UpdateNameGeo(Mongostring, dbname, context.Background(),
+					LonLatProperties{
+						Type:   resp.Type,
+						Name:   resp.Name,
+						Volume: resp.Volume,
+					})
+				req.Message = fmt.Sprintf("%v:%v", "Berhasil Update data", Ins)
+			}
+		}
+	}
+	return ReturnStringStruct(req)
+}
+
+func GCFDeleteLon(publickey, Mongostring, dbname string, r *http.Request) string {
+	req := new(Credents)
+	resp := new(LonLatProperties)
+	err := json.NewDecoder(r.Body).Decode(&resp)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		req.Status = strconv.Itoa(http.StatusNotFound)
+		req.Message = "Header Login Not Exist"
+	} else {
+		existing := IsExist(tokenlogin, os.Getenv(publickey))
+		if !existing {
+			req.Status = strconv.Itoa(http.StatusNotFound)
+			req.Message = "Kamu kayaknya belum punya akun"
+		} else {
+			if err != nil {
+				req.Status = strconv.Itoa(http.StatusNotFound)
+				req.Message = "error parsing application/json: " + err.Error()
+			} else {
+				req.Status = strconv.Itoa(http.StatusOK)
+				Ins := DeleteDataGeo(Mongostring, dbname, context.Background(),
+					LonLatProperties{
+						Type:   resp.Type,
+						Name:   resp.Name,
+						Volume: resp.Volume,
+					})
+				req.Message = fmt.Sprintf("%v:%v", "Berhasil Hapus data", Ins)
+			}
+		}
 	}
 	return ReturnStringStruct(req)
 }
@@ -41,42 +137,47 @@ func ReturnStringStruct(Data any) string {
 	return string(jsonee)
 }
 
-func GCFUpdateNameGeojson(Mongostring, dbname, colname string, r *http.Request) string {
-	req := new(Credents)
-	resp := new(LonLatProperties)
-	conn := GetConnectionMongo(Mongostring, dbname)
-	err := json.NewDecoder(r.Body).Decode(&resp)
+func Register(Mongoenv, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	userdata := new(RegisterStruct)
+	resp.Status = false
+	conn := GetConnectionMongo(Mongoenv, dbname)
+	err := json.NewDecoder(r.Body).Decode(&userdata)
 	if err != nil {
-		req.Status = strconv.Itoa(http.StatusNotFound)
-		req.Message = "error parsing application/json: " + err.Error()
+		resp.Message = "error parsing application/json: " + err.Error()
 	} else {
-		req.Status = strconv.Itoa(http.StatusOK)
-		Ins := UpdateDataGeojson(conn, colname,
-			resp.Name,
-			resp.Volume,
-			resp.Type)
-		req.Message = fmt.Sprintf("%v:%v", "Berhasil Update data", Ins)
+		resp.Status = true
+		hash, err := pasproj.HashPass(userdata.Password)
+		if err != nil {
+			resp.Message = "Gagal Hash Password" + err.Error()
+		}
+		InsertUserdata(conn, userdata.Username, hash)
+		resp.Message = "Berhasil Input data"
 	}
-	return ReturnStringStruct(req)
+	response := pasproj.ReturnStringStruct(resp)
+	return response
 }
 
-func GCFDeleteDataGeojson(Mongostring, dbname, colname string, r *http.Request) string {
-	req := new(Credents)
-	resp := new(LonLatProperties)
-	conn := GetConnectionMongo(Mongostring, dbname)
-	err := json.NewDecoder(r.Body).Decode(&resp)
+func Login(Privatekey, MongoEnv, dbname, Colname string, r *http.Request) string {
+	var resp pasproj.Credential
+	mconn := pasproj.MongoCreateConnection(MongoEnv, dbname)
+	var datauser peda.User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
 	if err != nil {
-		req.Status = strconv.Itoa(http.StatusNotFound)
-		req.Message = "error parsing application/json: " + err.Error()
+		resp.Message = "error parsing application/json: " + err.Error()
 	} else {
-		req.Status = strconv.Itoa(http.StatusOK)
-		delResult, delErr := DeleteDataGeojson(conn, colname, resp.Name)
-		if delErr != nil {
-			req.Status = strconv.Itoa(http.StatusInternalServerError)
-			req.Message = "error deleting data: " + delErr.Error()
+		if peda.IsPasswordValid(mconn, Colname, datauser) {
+			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(Privatekey))
+			if err != nil {
+				resp.Message = "Gagal Encode Token : " + err.Error()
+			} else {
+				resp.Status = true
+				resp.Message = "Selamat Datang"
+				resp.Token = tokenstring
+			}
 		} else {
-			req.Message = fmt.Sprintf("Berhasil menghapus data. Jumlah data terhapus: %v", delResult.DeletedCount)
+			resp.Message = "Password Salah"
 		}
 	}
-	return ReturnStringStruct(req)
+	return pasproj.ReturnStringStruct(resp)
 }
--- go.mod
+++ go.mod
@@ -3,16 +3,21 @@ module github.com/gisdaps/gcpbackends6
 go 1.21.3
 
 require (
+	github.com/HRMonitorr/PasetoprojectBackend v0.0.11
 	github.com/aiteung/atdb v0.1.1
+	github.com/petapedia/peda v0.1.1
+	github.com/whatsauth/watoken v0.1.3
 	go.mongodb.org/mongo-driver v1.12.1
 )
 
 require (
+	aidanwoods.dev/go-paseto v1.5.0 // indirect
+	aidanwoods.dev/go-result v0.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/klauspost/compress v1.17.1 // indirect
 	github.com/microsoft/go-mssqldb v1.0.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -21,5 +26,6 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
